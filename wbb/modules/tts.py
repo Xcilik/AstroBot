5bad1a2acf4f05a90b4404f022ae8eff7be1ad74
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-03-02 19:04:33 +0700

    Update tts.py

--- wbb/modules/tts.py
+++ wbb/modules/tts.py
@@ -21,43 +21,92 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import traceback
-from asyncio import get_running_loop
-from io import BytesIO
-
-from googletrans import Translator
-from gtts import gTTS
 from pyrogram import filters
-from pyrogram.types import Message
 
 from wbb import app
+import gtts
+from aiofiles.os import remove as aremove
+from gpytranslate import Translator
 
 
-def convert(text):
-    audio = BytesIO()
-    i = Translator().translate(text, dest="en")
-    lang = i.src
-    tts = gTTS(text, lang=lang)
-    audio.name = lang + ".mp3"
-    tts.write_to_fp(audio)
-    return audio
+__MODULE__ = "Translator"
+__HELP__ = f"""
+/tr [lang_code - reply/text]
+     To translate text.
+
+/tts [lang_code - reply/text]
+     Text To Voice.
+"""
 
 
 @app.on_message(filters.command("tts"))
-async def text_to_speech(_, message: Message):
-    if not message.reply_to_message:
-        return await message.reply_text("Reply to some text ffs.")
-    if not message.reply_to_message.text:
-        return await message.reply_text("Reply to some text ffs.")
-    m = await message.reply_text("Processing")
-    text = message.reply_to_message.text
+async def _(_, message):
+    if message.reply_to_message:
+        if len(message.command) < 2:
+            language = "id"
+            words_to_say = (
+                message.reply_to_message.text or message.reply_to_message.caption
+            )
+        else:
+            language = message.text.split(None, 2)[1]
+            words_to_say = (
+                message.reply_to_message.text or message.reply_to_message.caption
+            )
+    else:
+        if len(message.command) < 3:
+            return
+        else:
+            language = message.text.split(None, 2)[1]
+            words_to_say = message.text.split(None, 2)[2]
+    speech = gtts.gTTS(words_to_say, lang=language)
+    speech.save("text_to_speech.oog")
+    reply_me_or_user = message.reply_to_message or message
     try:
-        loop = get_running_loop()
-        audio = await loop.run_in_executor(None, convert, text)
-        await message.reply_audio(audio)
-        await m.delete()
-        audio.close()
-    except Exception as e:
-        await m.edit(e)
-        e = traceback.format_exc()
-        print(e)
+        await _.send_voice(
+            chat_id=message.chat.id,
+            voice="text_to_speech.oog",
+            reply_to_message_id=reply_me_or_user.id,
+        )
+    except:
+        ABC = await message.reply(
+            "Pesan Suara tidak diizinkan di sini.\nSalin yang dikirim ke Pesan Tersimpan."
+        )
+        await _.send_voice(_.me.id, "text_to_speech.oog")
+        await message.delete()
+        await ABC.delete()
+        await asyncio.sleep(2)
+    try:
+        await aremove("text_to_speech.oog")
+    except FileNotFoundError:
+        pass
+
+
+@app.on_message(filters.command("tr"))
+async def _(client, message):
+    trans = Translator()
+    if message.reply_to_message:
+        if len(message.command) < 2:
+            dest = "id"
+            to_translate = (
+                message.reply_to_message.text or message.reply_to_message.caption
+            )
+            source = await trans.detect(to_translate)
+        else:
+            dest = message.text.split(None, 2)[1]
+            to_translate = (
+                message.reply_to_message.text or message.reply_to_message.caption
+            )
+            source = await trans.detect(to_translate)
+    else:
+        if len(message.command) < 3:
+            return
+        else:
+            dest = message.text.split(None, 2)[1]
+            to_translate = message.text.split(None, 2)[2]
+            source = await trans.detect(to_translate)
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated\n    Language:</b> <code>{source}</code> to <code>{dest}</code>\n    <code>{translation.text}</code>"
+    reply_me_or_user = message.reply_to_message or message
+    await client.send_message(
+        message.chat.id, reply, reply_to_message_id=reply_me_or_user.id
+            )
